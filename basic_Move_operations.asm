28ce2a9159bf8a57b6f833b5ade95cc5cd0046b3
jonahjason0825 <jonahjason9304@gmail.com>
2025-11-09 21:34:58 +0530

    Delete files

--- basic_Move_operations.asm
+++
@@ -1,5 +0,0 @@
-ORG 00H
-MOV R1, #03H; stores hexadecimal value of 3 at R1 register
-MOV R2, #42H; stores hexadecimal value of 42 at R2 register
-MOV R3, #17H; stores hexadecimal value of 17 at R3 register
-END ;this is very basic code, you may experiment with different values of numbers as well as different registers in the range of R0-R7, A, and B.
