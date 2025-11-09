77038b24eac269d8c28cb233da86b87331835cf2
jonahjason0825 <jonahjason9304@gmail.com>
2025-11-09 21:35:12 +0530

    Delete basic_addition.asm

--- basic_addition.asm
+++
@@ -1,5 +0,0 @@
-ORG 00H
-MOV A, #07H; moves hexadecimal value of 7 to accumulator
-MOV B, #05H; moves hexadecimal value of 5 to register B
-ADD A, B; adds the values and stores the result in accumulator
-END
